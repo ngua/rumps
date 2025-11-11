33c1b843b0a65790fb7d428fc02deb09f7dfd88b
Rory Tyler Hayford <rory.hayford@protonmail.com>
2025-11-11 11:52:19 +0700

    Delete old stuff

--- crates/rumps/src/command.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum Command {}
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
-pub struct Global<T>(pub(crate) T);
